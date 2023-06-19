6eaf5a522ffe8a3d810dae86cd99f77239a25aba
samyog437 <fallendemon174@gmail.com>
2023-06-19 21:18:30 +0545

    created project

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { Space } from "antd";
+import { Nav, Navbar } from "react-bootstrap";
+
+const Header = (props) => {
+  return (
+    <Navbar
+      bg="light"
+      expand="lg"
+      style={{ padding: "10px 40px", backgroundColor: "#ad5389" }}
+      className="navigation-bar"
+    >
+      <Link to={"/"} className="no-link">
+        <div className="navbar-logo">AgriMart</div>
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Link to="/" className="no-link" style={{ marginRight: "20px" }}>
+            Home
+          </Link>
+          <Link to="/login" className="no-link">
+            Login
+          </Link>
+        </Nav>
+
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
