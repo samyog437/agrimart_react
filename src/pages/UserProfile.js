85af3d4c39de3991afcdab952af297de00db71bb
samyog437 <fallendemon174@gmail.com>
2023-07-12 20:12:30 +0545

    styling to profile page

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import profilethumb from "../assets/images/profile-user.png";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
-import { Col, Row } from "antd";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Button, Col, Row } from "antd";
 import ProductCard from "../components/ProductCard";
 
 const UserProfile = (props) => {
     const location = useLocation();
+    const navigate = useNavigate();
     const path = location.pathname.split("/")[2];
     const [userData, setUserData] = useState();
     const token = localStorage.getItem('token');
+    const publicFolder = "http://localhost:5000/image/"
 
     const config = { headers: {Authorization: `Bearer ${token}`} }
 
@@ -33,47 +35,79 @@ const UserProfile = (props) => {
         fetchDeliveryData();
     }, []);
 
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <>
             <div className="text-center">
-                <h1>Profile Page</h1>
+                <h3>Profile Details</h3>
                 <div className="profile-img">
                     <img src={profilethumb} alt="Profile" />
                 </div>
                 {userData && (
                 <div className="user-info">
-                    <p>{userData.username}</p>
-                    <p>Full Name: {userData.fullname}</p>
-                    <p>Email: {userData.email}</p>
-                    <h2>My Orders</h2>
-                    <div className="cart-content">
-                        <div className="cart-body">
-
-                        </div>
+                    <div className="user-info-data">
+                        <p className="user-info-username">{userData.username}</p>
+                        <p>Full Name: {userData.fullname}</p>
+                        <p>Email: {userData.email}</p>
                     </div>
-                    {userData.deliveries && userData.deliveries.length > 0 ? (
+                    <h3>My Orders</h3>
+                    <div className="cart-content order-content">
+                        <div className="cart-body order-body">
+                        {userData.deliveries && userData.deliveries.length > 0 ? (
                         userData.deliveries.map(delivery => (
                             <div className="cart-list" key={delivery._id}>
-                                
-                                    {delivery.products.map(product => (
+                                <div className="order-item-parent">
+                                    {delivery.products.map((product, index) => (
                                         <>
-                                            <div className="cart-item-left">
-                                                <div key={product._id}>
+                                        <div className="cart-item order-item" key={product._id}>
+                                            <div className="cart-item-left order-item-left">             
+                                                <img 
+                                                className="product-image"
+                                                src={publicFolder + product.productId.image} alt="product" style={{
+                                                width: "100px",
+                                                height: "70px",
+                                                objectFit: "cover",
+                                                 }} />
+                                                <div className="cart-name">                                            
                                                     {product.productId.title}
                                                 </div>
                                             </div>
-                                            <div className="cart-item-right">
-                                                {/* <div key={product._id}>
-                                                    {product.productId}
-                                                </div> */}
-                                            </div>
+                                        </div>
+                                        {index !== (delivery.products.length - 1) && <hr/>}
                                         </>
+                                        
                                     ))}
+                                </div>
+                                    <div className="order-item-parent">
+                                        {delivery.products.map(product => (
+                                            <div className="cart-item" key={product._id}>
+                                                <div className="cart-item-right order-item-right">
+                                                        Rs.{product.productId.price}
+                                                </div>
+                                            </div>                                     
+                                        ))}
+                                        
+                                        <div className="total-price">
+                                            Total Price: Rs.{delivery.totalPrice}
+                                        </div>
+                                    </div>
                             </div>
                         ))
                     ) : (
                         <p>No deliveries found.</p>
                     )}
+                        </div>
+                    </div>
+                    <div className="go-back-button">
+                        <Button onClick={handleGoBack} className="back-btn" type="primary" style={{
+                            backgroundColor: "#F54040"
+                        }} danger>
+                            Go Back
+                        </Button>
+                    </div>
                 </div>
                 )}
             </div>
